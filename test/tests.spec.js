0101eb1fd416ecebbca6b8441ab4078c4556680c
Timo Liebetrau <t.liebetrau@litixsoft.de>
2013-03-28 15:01:58 +0100

    jasmine async test

--- test/tests.spec.js
+++ test/tests.spec.js
@@ -1,10 +1,30 @@
-describe('lx-mongodb', function () {
+/*global describe, it, expect, runs, waitsFor */
+describe('MongoJs specs', function () {
     'use strict';
+    var bbDb = require('../lib/lx-mongodb'),
+        blogConnection = 'localhost/blog?w=1&journal=True&fsync=True',
+        db = bbDb.GetDb(blogConnection, ['users', 'posts', 'tags', 'categories', 'comments']),
+        value,
+        flag;
 
-    it('test for data access', function () {
-        var result = true;
+    it('should support async execution of test preparation and exepectations', function () {
+        runs(function () {
+            flag = false;
+            value = 0;
 
-        expect(result).toBe(true);
+            db.users.find({userName: 'diiimo_7'}, function (err, res) {
+                console.log(res[0].email);
+                value = res[0].email;
+                flag = true;
+            });
+        });
 
+        waitsFor(function () {
+            return flag;
+        });
+
+        runs(function () {
+            expect(value).toBe('diiimo_7@gmail.com');
+        });
     });
 });
